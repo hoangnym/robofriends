3213291bc1536aaea7d5b4ff391a09ec3553fb3c
Hoang <hoangnym94@gmail.com>
2022-05-30 14:39:04 +0200

    fourth commit

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <h1> Hello World </h1>
-    <App />
+    <Hello />
   </React.StrictMode>
-);
+)
+
+//root.render( <React.StrictMode> <App /> </React.StrictMode> );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
