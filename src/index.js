cce43a5da820d094b54d5f5b3923722637e281f0
Hoang <hoangnym94@gmail.com>
2022-05-30 14:41:38 +0200

    add tachyons

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
+import "tachyons";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
