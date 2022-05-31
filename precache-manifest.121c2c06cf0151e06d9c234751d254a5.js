ff02a66167bd7dee1344f85f1be17894a5ec1911
Hoang <hoangnym94@gmail.com>
2022-05-31 09:03:48 +0200

    Updates

---
+++ precache-manifest.121c2c06cf0151e06d9c234751d254a5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c846cfb00bf205cb9fd8",
+    "url": "/robofriends/static/css/main.ac4ea6b8.chunk.css"
+  },
+  {
+    "revision": "c846cfb00bf205cb9fd8",
+    "url": "/robofriends/static/js/main.5092af88.chunk.js"
+  },
+  {
+    "revision": "75dcfc39fa8879b648fd",
+    "url": "/robofriends/static/js/runtime~main.e6791cfe.js"
+  },
+  {
+    "revision": "89b51f50e9025b36b09f",
+    "url": "/robofriends/static/css/2.c6d67a4d.chunk.css"
+  },
+  {
+    "revision": "89b51f50e9025b36b09f",
+    "url": "/robofriends/static/js/2.e009e008.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/robofriends/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "4cb51c76d7d374b1f1e13b15d1673cec",
+    "url": "/robofriends/index.html"
+  }
+];
